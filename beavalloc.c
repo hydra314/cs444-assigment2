34ad4f3b4af08b4975ad0297c81c8606446ad052
hydra314 <multi.goals@gmail.com>
2019-10-07 16:24:12 -0700

    initialize repo

---
+++ beavalloc.c
@@ -0,0 +1,38 @@
+#ifndef MEMREQUEST
+#  define MEMREQUEST 1024
+#endif   // MEMREQUEST
+
+void *beavalloc(size_t size)
+{
+   
+}
+
+void beavfree(void *ptr)
+{
+
+}
+
+void beavalloc_reset(void)
+{
+
+}
+
+void beavalloc_set_verbose(uint8_t)
+{
+
+}
+
+void *beavcalloc(size_t nmemb, size_t size)
+{
+
+}
+
+void *beavrealloc(void *ptr, size_t size)
+{
+
+}
+
+void beavalloc_dump(uint leaks_only)
+{
+
+}
